846129ad2269ad876bef3ea8edb5471ed1270e0f
adarshkunwar <adarshkunwarofficial@gmail.com>
2026-06-14 21:28:54 +0545

    feat: animation works on both sides

--- src/player.c
+++ src/player.c
@@ -1,9 +1,13 @@
 #include "player.h"
 
 const char *idle_sprite[] = {" o ", "/|\\", "/ \\"};
-const char *walk_frame1[] = {" o ", "/|\\", "/ \\"};
-const char *walk_frame2[] = {" o ", "\\|", "> |"};
-const char *walk_frame3[] = {" o ", "/|\\", "| >"};
+const char *walk_right_frame1[] = {" o ", "/|\\", "/ \\"};
+const char *walk_right_frame2[] = {" o ", "/||", "> |"};
+const char *walk_right_frame3[] = {" o ", "||\\", "| >"};
+
+const char *walk_left_frame1[] = {" o ", "/|\\", "/ \\"};
+const char *walk_left_frame2[] = {" o ", "|||", "< |"};
+const char *walk_left_frame3[] = {" o ", "/|\\", "| <"};
 
 void init_player(Player *p) {
   p->pos.x = 10;
@@ -27,12 +31,22 @@ void move_player(Player *p, int dx, int dy) {
     if (p->walk_tick >= 6)
       p->walk_tick = 0;
 
-    if (p->walk_tick < 2) {
-      p->current_sprite = walk_frame1;
-    } else if (p->walk_tick < 4) {
-      p->current_sprite = walk_frame2;
+    if (dx > 0) {
+      if (p->walk_tick < 2) {
+        p->current_sprite = walk_right_frame1;
+      } else if (p->walk_tick < 4) {
+        p->current_sprite = walk_right_frame2;
+      } else {
+        p->current_sprite = walk_right_frame3;
+      }
     } else {
-      p->current_sprite = walk_frame3;
+      if (p->walk_tick < 2) {
+        p->current_sprite = walk_left_frame1;
+      } else if (p->walk_tick < 4) {
+        p->current_sprite = walk_left_frame2;
+      } else {
+        p->current_sprite = walk_left_frame3;
+      }
     }
   } else {
     p->current_sprite = idle_sprite;
--- src/player.h
+++ src/player.h
@@ -2,9 +2,13 @@
 #define PLAYER_H
 
 extern const char *idle_sprite[]; 
-extern const char *walk_frame1[]; 
-extern const char *walk_frame2[]; 
-extern const char *walk_frame3[]; 
+extern const char *walk_right_frame1[]; 
+extern const char *walk_right_frame2[]; 
+extern const char *walk_right_frame3[]; 
+
+extern const char *walk_left_frame1[]; 
+extern const char *walk_left_frame2[]; 
+extern const char *walk_left_frame3[]; 
 
 typedef struct {int x; int y;} Position;
 typedef struct {int will; int san; int max_will; int max_san;} Stats;
